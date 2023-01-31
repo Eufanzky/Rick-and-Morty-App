7ab2439fcbf1d26a83b62dbb1eaef21ded70b658
eufanzky <eufanzky@gmail.com>
2023-01-30 21:04:16 -0400

    added new code to start with

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState, useEffect, useContext } from "react";
+//import { Nav } from "./components/Nav/Nav";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [characters, setCharacters] = useState("");
+
+  const urlCharacters = "https://rickandmortyapi.com/api/character";
+
+
+  async function getDataFromAPI(url) {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error(error); 
+    }
+  }
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <>
+      <header className="header">
+      </header>
+
+      <main className="main">
+        <h1 className="main-title">Rick and Morty App</h1>
+        <section className="characters-section">
+          <h2>Characters</h2>
+          <div></div>
+        </section>
+      </main>
+
+      <footer className="footer">
+
+      </footer>
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,13 @@
+import "./Nav.css";
+
+function Nav() {
+    return <nav>
+        <ul>
+            <li>Home</li>
+            <li>Characters</li>
+            <li>Episodes</li>
+            <li>About</li>
+        </ul>
+    </nav>
+}
+export default Nav;
