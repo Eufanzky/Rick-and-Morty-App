070fab96c1c910a6ad17770208b8dabebb71a4af
eufanzky <eufanzky@gmail.com>
2023-03-03 17:36:30 -0400

    commit before changing files structure

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -4,7 +4,6 @@ function Nav() {
     return <nav className="nav">
         <ul className="nav-ul">
             <li className="nav-elements">Home</li>
-            <li className="nav-elements">Episodes</li>
             <li className="nav-elements">About</li>
             <li className="nav-elements">Game</li>
         </ul>
