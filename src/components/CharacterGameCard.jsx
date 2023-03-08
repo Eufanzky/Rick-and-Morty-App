72180f94d77b1fddbcda445f525c5ca54cdbdb0b
eufanzky <eufanzky@gmail.com>
2023-03-07 21:25:41 -0400

    ADDED new animations whe clicking game cards

--- src/components/CharacterGameCard.jsx
+++ src/components/CharacterGameCard.jsx
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/CharacterGameCard.scss";
 
 export const CharacterGameCard = ({ image }) => {
+  const [isFlipped, setIsFlipped] = useState(false);
+  let [numberOfClicks, setNumberOfClicks] = useState(0);
+
+  const handleClick = (event) => {
+    setIsFlipped(!isFlipped);
+    console.log(numberOfClicks);
+    console.log(event.target);
+    if (numberOfClicks === 2) {
+      console.log("dos clicks :o");
+    } else {
+      setNumberOfClicks(numberOfClicks++);
+    }
+  };
+
   return (
-    <div className="game-card-image-div">
-      <img src={image} />
+    <div
+      className={`game-card ${isFlipped ? "flipped" : ""}`}
+      onClick={handleClick}
+    >
+      <div className="game-card-image-div-front">
+        <img src={image} alt="front" />
+      </div>
+      <div className="game-card-image-div-back">
+        <img
+          src="https://media.istockphoto.com/id/1263395015/es/foto/lupa-y-marca-de-pregunta-sobre-fondo-p%C3%BArpura.jpg?s=1024x1024&w=is&k=20&c=a9w-OvmwVo8XhwIVf8gcllP6p2oY-v66IVJHG0-iyzc="
+          alt="back"
+        />
+      </div>
     </div>
   );
 };
